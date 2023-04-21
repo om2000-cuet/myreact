fa7004120df17def4a4a43a49ce3144a324086f3
om2000-cuet <prakash02cuet@gmail.com>
2023-04-21 16:35:56 +0600

    button with route created

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import  { createRoot } from 'react-dom';
 
 function App() {
+  const navigate=useNavigate();
+  const navigateToContacts=()=>{
+    navigate('/contacts');
+  }
+const navigateHome=()=>{
+  navigate('/');
+}
+
+
   return (
-    <div>Hello, World!</div>
+    <div>
+      <button onClick={navigateHome}>Home</button>
+      <button onClick={navigateToContacts}>Contacts</button>
+      <Routes>
+        <Route path="/contacts" element={<Contacts/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+
+
+
+
+      </div>
+  );
+}
+function Home(){
+  return(<div>
+  <h2>Hello World</h2>
+</div>
+
+  );
+}
+function Contacts(){
+  return(
+    <h2>i am in Contact</h2>
   );
 }
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react'
-              ]
-            }
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
           }
         }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html'
-      })
-    ],
-    devServer: {
-      port: 3000
-    }
-  };
-  
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    port: 3000
+  }
+};
